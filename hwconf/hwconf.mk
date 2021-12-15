84ef55f9e9886451483c5cc803d8ff171321aeed
Clemens Elflein <clemens1@familie-elflein.de>
2021-12-16 00:16:58 +0100

    xESC wip

--- hwconf/hwconf.mk
+++ hwconf/hwconf.mk
@@ -4,7 +4,8 @@ HWSRC =	hwconf/hw.c \
 	hwconf/drv8320s.c \
 	hwconf/drv8323s.c \
 	hwconf/luna/luna_display_serial.c \
-	hwconf/si8900.c
+	hwconf/si8900.c \
+	hwconf/tmc6200.c
 
 HWINC = hwconf \
 		hwconf/luna
